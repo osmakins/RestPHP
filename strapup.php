e3ff5e5cd41cc75fa9c790e20b922b3646f106e0
Matthew Akindayo <matthewakinse@gmail.com>
2021-05-11 20:48:56 +0300

    Set up database connection

--- strapup.php
+++ strapup.php
@@ -1,11 +1,10 @@
 <?php
 require 'vendor/autoload.php';
+
 use Dotenv\Dotenv;
+use Src\System\dbconn;
 
 $dotenv = new DotEnv(__DIR__);
 $dotenv->load();
 
-// test code, should output:
-// api://default
-// when you run $ php bootstrap.php
-echo getenv('OKTAAUDIENCE');
+$dbConnection = (new DatabaseConnector())->getConnection();
