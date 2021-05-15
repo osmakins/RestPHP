5e380e27cd27ddabb0d624c29fc05601a93439bc
Matthew Akindayo <matthewakinse@gmail.com>
2021-05-15 10:30:30 +0300

    Add database table gateway

---
+++ src/TableGateways/PersonGateway.php
@@ -0,0 +1,45 @@
+<?php
+namespace Src\TableGateways;
+
+class PersonGateway{
+    private $db = null;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function findAll(){
+        $statement = "
+            SELECT
+                id, firstname, lastname, firstparent_id, secondparent_id
+            FROM
+                person;
+        ";
+        try{
+            $statement = $this->db->query($statement);
+            $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            return $result;
+        } catch (\PDOException $e){
+            exit($e->getMessage());
+        }
+    }
+
+    public function find($id){
+        $statement = "
+            SELECT
+                id, firstname, lastname, firstparent_id, secondparent_id
+            FROM
+                person
+            WHERE id=?;
+        ";
+        try{
+            $statement = $this->db->prepare($statement);
+            $statement->execute(array($id));
+            $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            return $result;
+        }catch(\PDOException $e){
+            exit($e->getMessage());
+        }
+    }
+}
