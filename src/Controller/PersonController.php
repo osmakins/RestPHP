4169d3a8e29dc6f0c2b1288b69e80d1b8140252e
Matthew Akindayo <matthewakinse@gmail.com>
2021-05-19 23:51:27 +0300

    Update person controller

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -21,7 +21,29 @@ class PersonController{
 
     public function processRequest(){
         switch($this->requestMethod){
-            
+            case 'GET':
+                if($this->userId){
+                    $response = $this->getUser($this->userId);
+                }else{
+                    $response = $this->getAllUsers();
+                };
+                break;
+            case 'POST':
+                $response = $this->createUserFromRequest();
+                break;
+            case 'PUT':
+                $response = $this->updateUserFromRequest($this->userId);
+                break;
+            case 'DELETE':
+                $response = $this->deleteUser($this->userId);
+                break;
+            default:
+                $response = $this->notFoundResponse();
+                break;
+        }
+        header($response['status_code_header']);
+        if($response['body']){
+            echo $response['body'];
         }
     }
 }
