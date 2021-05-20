2f8018fa9dd2c0f3c28b88436ad73bb975717526
Matthew Akindayo <matthewakinse@gmail.com>
2021-05-20 09:14:05 +0300

    Update person gateway methods

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -53,23 +53,75 @@ class PersonController{
         return $response;
     }
 
-    private function getUser(){
-        
+    private function getUser($id){
+        $result = $this->personGateway->find($id);
+        if(!$result){
+            return $this->notFoundResponse();
+        }
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
     }
 
     private function createUserFromRequest(){
-
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+        if(! $this->validatePerson($input)){
+            return $this->unprocessableEntityResponse();
+        }
+        $this->personGateway->insert($input);
+        $response['status_code_header'] = 'HTTP/1.1 201 Created';
+        $response['body'] = null;
+        return $response;
     }
 
     private function updateUserFromRequest($id){
-
+        $result = $this->personGateway->find($id);
+        if(! $result){
+            return $this->notFoundResponse();
+        }
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+        if(! $this->validatePerson($input)){
+            return $this->unprocessableEntityResponse();
+        }
+        $this->personGateway->update($id, $input);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = null;
+        return $response;
     }
 
     private function deleteUser($id){
-        
+        $result = $this->personGateway->find($id);
+        if(! $result){
+            return $this->notFoundResponse();
+        }
+        $this->personGateway->delete($id);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = null;
+        return $response;
     }
 
-    private function notFoundResponse(){
+    private function validatePerson(){
+        if(! isset($input['firstname'])){
+            return false;
+        }
+        if(! isset($input['lastname'])){
+            return false;
+        }
+        return true;
+    }
 
+    private function unprocessableEntityResponse(){
+        $response['status_code_header'] = 'HTTP/1.1 422 Unprocessable Entity';
+        $response['body'] = json_encode([
+            'error' => 'invalid input'
+        ]);
+        return $response;
+    }
+
+
+    private function notFoundResponse(){
+        $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+        $response['body'] = null;
+        return $response;
     }
 }
